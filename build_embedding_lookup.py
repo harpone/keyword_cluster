f2fade0dda3faf6b546af49e8a8999845fa3687d
heka <heikki.a.arponen@gmail.com>
2018-11-20 16:33:37 +0200

    `build_embedding_lookup.py` and `find_similar.py` drafts

---
+++ build_embedding_lookup.py
@@ -0,0 +1,7 @@
+
+
+
+"""
+Load a trained model, push dataset through it and save the embeddings together with the X, Y
+
+"""
---
+++ find_similar.py
@@ -0,0 +1,5 @@
+
+
+"""
+Load saved model, take user input and find similar reddit topics based on similarity in embedding space.
+"""
