d55bb0d84b80f5892d90f725eb60fd688b2199dd
heka <heikki.a.arponen@gmail.com>
2018-11-21 12:37:45 +0200

    TODOs, readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Notes
 
+TODO: more details here
+
 * first run `extract_image_posts.py` to get only posts containing links to images
 * then `create_vocabulary.py` will dump the vocabulary and label dictionary as json files to project root
 * then `train_classifier.py`
+* spacy installation requires the english language pack, which can be downloaded by `python -m spacy download en
+`
--- extract_image_posts.py
+++ extract_image_posts.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from os.path import join
+from os.path import join, isfile
 import ast
 
 path_root = '/mnt/TERA/Data/reddit_topics'
 path_data = join(path_root, 'safe_links_all')
+out_path = join(path_root, 'img_reddits.csv')
 
 image_types = ['.jpg', '.png']
 df = []
@@ -19,12 +20,18 @@ with open(path_data, 'r') as f:
             continue
         if any([x in lst[2] for x in image_types]):
             df.append(lst)
-        if len(df) > 100000:  # testing with a lot smaller dataset first
-            break
+        if len(df) > 1000000:  # memory efficient
+            df = pd.DataFrame(df)
+            df.columns = ['subreddit', 'submission_title', 'submission_link', 'comments_link', 'short_name']
+            if not isfile(out_path):
+                df.to_csv(out_path, index=False)
+            else:
+                df.to_csv(out_path, mode='a', header=False, index=False)
+            df = []
 
-df = pd.DataFrame(df)
-df.columns = ['subreddit', 'submission_title', 'submission_link', 'comments_link', 'short_name']
-df.head()
+# df = pd.DataFrame(df)
+# df.columns = ['subreddit', 'submission_title', 'submission_link', 'comments_link', 'short_name']
+# df.head()
 
 # Save as CSV:
-df.to_csv(join(path_root, 'img_reddits.csv'), index=False)
+#df.to_csv(out_path, index=False)
