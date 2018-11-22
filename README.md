44daf824dda311fe6c7985abc25a7830b14a52af
heka <heikki.a.arponen@gmail.com>
2018-11-22 13:25:10 +0200

    argparse in extract_image_posts and readme

--- README.md
+++ README.md
@@ -6,17 +6,21 @@ layer as an embedding and to measure similarity between titles
 by nearest neighbor distance in the embedding space.
 
 Current classifier is a fully connected neural network, and it feeds
-on lemmatized and count-vectorized strings (reddit post titles).
- Future plans involve 1) simple char-rnn classifier 2) using e.g.
- pretrained ELMo embeddings instead of training a classifier. 
+on lemmatized and count-vectorized strings (reddit post titles). 
+There's also ELMo embeddings as a baseline.
+
+I actually regret a bit not implementing a simple character 
+level RNN, since the title strings are quite short and there's
+lots of data, so the char-rnn classifier would probably be able
+to learn pretty good embeddings, and it would require virtually
+no preprocessing... now I'm having lots of slowness problems 
+with tokenizing, lemmatizing and buildint the vocabulary for the
+resnet classifier. 
  
 # Instructions
 
-So far no CLI args. See inside the scripts if you want to change
-the arguments.
 
-* spacy installation requires the english language pack, which can be downloaded by `python -m spacy download en`
-* first run `extract_image_posts.py` to get only posts containing links to images
+* run `extract_image_posts.py` to get only posts containing links to images
 * then `create_vocabulary.py` will dump the vocabulary and label dictionary as json files to project root
 * then `train_classifier.py`
 * then run `build_embedding_lookup.py`, which will include the 
--- extract_image_posts.py
+++ extract_image_posts.py
@@ -1,37 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import argparse
 from os.path import join, isfile
 import ast
 
-path_root = '/mnt/TERA/Data/reddit_topics'
-path_data = join(path_root, 'safe_links_all')
-out_path = join(path_root, 'img_reddits.csv')
 
-image_types = ['.jpg', '.png']
-df = []
-with open(path_data, 'r') as f:
-    for n, line in enumerate(f):
-        # print(f'\r{n}', end='')
-        try:
-            lst = ast.literal_eval(line)  # [subreddit, submission title, submitted link, comments link, short name]
-        except ValueError:
-            # print('ValueError')
-            continue
-        if any([x in lst[2] for x in image_types]):
-            df.append(lst)
-        if len(df) > 1000000:  # memory efficient
-            df = pd.DataFrame(df)
-            df.columns = ['subreddit', 'submission_title', 'submission_link', 'comments_link', 'short_name']
-            if not isfile(out_path):
-                df.to_csv(out_path, index=False)
-            else:
-                df.to_csv(out_path, mode='a', header=False, index=False)
-            df = []
+def main(path_data=None,
+         out_path=None):
+    #path_root = '/mnt/TERA/Data/reddit_topics'
+    #path_data = join(path_root, 'safe_links_all')
+    #out_path = join(path_root, 'img_reddits.csv')
 
-# df = pd.DataFrame(df)
-# df.columns = ['subreddit', 'submission_title', 'submission_link', 'comments_link', 'short_name']
-# df.head()
+    image_types = ['.jpg', '.png']
+    df = []
+    with open(path_data, 'r') as f:
+        for n, line in enumerate(f):
+            # print(f'\r{n}', end='')
+            try:
+                lst = ast.literal_eval(line)  # [subreddit, submission title, submitted link, comments link, short name]
+            except ValueError:
+                # print('ValueError')
+                continue
+            if any([x in lst[2] for x in image_types]):
+                df.append(lst)
+            if len(df) > 1000000:  # memory efficient
+                df = pd.DataFrame(df)
+                df.columns = ['subreddit', 'submission_title', 'submission_link', 'comments_link', 'short_name']
+                if not isfile(out_path):
+                    df.to_csv(out_path, index=False)
+                else:
+                    df.to_csv(out_path, mode='a', header=False, index=False)
+                df = []
 
-# Save as CSV:
-#df.to_csv(out_path, index=False)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create vocabulary for the resnet model')
+    parser.add_argument('-path_data',
+                        help='Path to the dataset ("safe_links_all")',
+                        default='./safe_links_all')
+    parser.add_argument('-out_path',
+                        help='Output path where the processed dataset will be saved',
+                        default='./img_reddits.csv')
+    args = parser.parse_args()
+
+    main(**vars(args))
+
